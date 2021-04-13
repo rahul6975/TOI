15c527b73c70760fd3d61acaf6eb4eb7fef67095
rahul6975 <rahulya7569@gmail.com>
2021-04-13 13:22:08 +0530

    Bottom navigation menu added

--- app/src/main/java/com/rahul/toi/views/HomeScreen.kt
+++ app/src/main/java/com/rahul/toi/views/HomeScreen.kt
@@ -1,11 +1,12 @@
-package com.rahul.toi
+package com.rahul.toi.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rahul.toi.R
 
-class MainActivity : AppCompatActivity() {
+class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_screen)
     }
 }
