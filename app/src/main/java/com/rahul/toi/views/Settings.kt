a40fe2d32bfe83984c0e3d4ebfd58f03b374f654
praveen <praveenaade15@gmail.com>
2021-04-17 13:44:06 +0530

    Edit Profile, Login Setting Activity, Fragment,splash screen

---
+++ app/src/main/java/com/rahul/toi/views/Settings.kt
@@ -0,0 +1,12 @@
+package com.rahul.toi.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rahul.toi.R
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
